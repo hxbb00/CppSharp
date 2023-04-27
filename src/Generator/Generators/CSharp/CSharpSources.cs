97d3200d0d07a08f7a9079e9e329ae240125f9cb
hujin <hj@mapgis.com>
2023-04-27 14:32:52 +0800

    虚析构函数

--- src/Generator/Generators/CSharp/CSharpSources.cs
+++ src/Generator/Generators/CSharp/CSharpSources.cs
@@ -2331,11 +2331,11 @@ internal static bool {Helpers.TryGetNativeToManagedMappingIdentifier}(IntPtr nat
                     else
                         Indent();
                     if (dtor.IsVirtual)
-{
-                            // this.GenerateMember(@class, c => GenerateDestructorCall(
-                            // c is ClassTemplateSpecialization ?
-                            //     c.Methods.First(m => m.InstantiatedFrom == dtor) : dtor));
-}
+                    {
+                        this.GenerateMember(@class, c => GenerateDestructorCall(
+                        c is ClassTemplateSpecialization ?
+                            c.Methods.First(m => m.InstantiatedFrom == dtor) : dtor));
+                    }
                     else
                         this.GenerateMember(@class, c => GenerateMethodBody(c, dtor));
                     if (@class.IsDependent || dtor.IsVirtual)
